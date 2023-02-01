c45142e2c343b00a4a0dc9ed09370e0d1e28151c
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-02-01 04:52:53 -0800

    added error handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,30 @@
+<script>
+  import './global.css'
+</script>
+
+<main class="card header">
+  <h1>Whoops, something went wrong.</h1>
+
+  <p data-color="red">
+    Try going back <a class=".accent-color" href="/">home</a> or use the
+    links above to find what you are looking for.
+  </p>
+</main>
+
+<style>
+  .card {
+    background-color: hsla(var(--hue), 100%, 45%, 0.1);
+    --spacing: 2rem;
+    padding: var(--spacing);
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing);
+    color: var(--secondary);
+    text-decoration: none;
+    height: calc(100vh - 6rem);
+  }
+
+  a {
+    color: var(--color);
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -47,7 +47,9 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: '404.html'
+    }),
     prerender: {
       handleMissingId: 'warn'
     }
